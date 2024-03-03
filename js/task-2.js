394f0f3163a2b818dd5fe52af4ba12904ba4413e
Viktor Ignatiev <viktor.ignatiev@wp.pl>
2024-03-03 11:14:32 +0100

    Home Work 2 vol. I

---
+++ js/task-2.js
@@ -0,0 +1,20 @@
+'use strict'
+
+function formatMessage(message, maxLength) {
+    const totalMessage = message.length;
+   
+    if (totalMessage <= maxLength) {
+        return message
+    } else if (totalMessage >= maxLength) {
+        return `${message.slice(0, maxLength)}...`
+    }
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+
